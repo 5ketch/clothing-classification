3550551e68b2db4ccc39d3decd17fb3150559ab8
5ketch <cassandra.p.chan@gmail.com>
2023-07-20 18:23:01 -0400

    removed unecessary code

--- clothing_classifier.py
+++ clothing_classifier.py
@@ -1,16 +1,13 @@
-#!/usr/bin/env python3
-
 import sys
 import argparse
 
 from jetson_inference import imageNet
-from jetson_utils import videoSource, videoOutput, cudaFont, Log
+from jetson_utils import videoSource, videoOutput, cudaFont, Log, loadImage
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("input", type=str, default="", nargs='?', help="URI of the input stream")
 parser.add_argument("output", type=str, default="", nargs='?', help="URI of the output stream")
-parser.add_argument("--topK", type=int, default=1, help="show the topK number of class predictions (default: 1)")
 
 try:
 	args = parser.parse_known_args()[0]
@@ -21,24 +18,21 @@ except:
 
 net = imageNet(model="model/resnet18.onnx", labels="model/labels.txt", input_blob="input_0", output_blob="output_0")
 
+img = loadImage(args.input)
+
 input = videoSource(args.input, argv=sys.argv)
 output = videoOutput(args.output, argv=sys.argv)
+font = cudaFont()
 
-while True:
-    img = input.Capture()
+classID, confidence = net.Classify(img)
 
-    if img is None:
-        continue  
+classLabel = net.GetClassLabel(classID)
+confidence *= 100.0
 
-    predictions = net.Classify(img, topK=args.topK)
+print(f"clothing type: {confidence:05.2f}% {classLabel}")
 
-    for n, (classID, confidence) in enumerate(predictions):
-        classLabel = net.GetClassLabel(classID)
-        confidence *= 100.0
-
-        print(f"clothing type: {confidence:05.2f}% {classLabel}")
+font.OverlayText(img, text=f"{confidence:05.2f}% {classLabel}")
                          
-    output.Render(img)
+output.Render(img)
 
-    if not input.IsStreaming() or not output.IsStreaming():
-        break
+
