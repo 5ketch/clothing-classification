cb75f5316e2d1939371c607fd4f45938ba3cbd95
5ketch <cassandra.p.chan@gmail.com>
2023-07-21 01:49:42 -0400

    added automatic sorting of outputs into folders

--- clothing_classifier.py
+++ clothing_classifier.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+import os
 
 from jetson_inference import imageNet
 from jetson_utils import videoSource, videoOutput, cudaFont, Log, loadImage
@@ -7,32 +8,50 @@ from jetson_utils import videoSource, videoOutput, cudaFont, Log, loadImage
 parser = argparse.ArgumentParser()
 
 parser.add_argument("input", type=str, default="", nargs='?', help="URI of the input stream")
-parser.add_argument("output", type=str, default="", nargs='?', help="URI of the output stream")
 
 try:
-	args = parser.parse_known_args()[0]
+    args = parser.parse_known_args()[0]
 except:
-	print("")
-	parser.print_help()
-	sys.exit(0)
+    print("")
+    parser.print_help()
+    sys.exit(0)
 
 net = imageNet(model="model/resnet18.onnx", labels="model/labels.txt", input_blob="input_0", output_blob="output_0")
 
-img = loadImage(args.input)
-
 input = videoSource(args.input, argv=sys.argv)
-output = videoOutput(args.output, argv=sys.argv)
 font = cudaFont()
 
-classID, confidence = net.Classify(img)
+if not os.path.exists("outputs/dress"):
+    os.makedirs("outputs/dress")
+if not os.path.exists("outputs/shirt"):
+    os.makedirs("outputs/shirt")
+if not os.path.exists("outputs/shorts"):
+    os.makedirs("outputs/shorts")
+if not os.path.exists("outputs/shoes"):
+    os.makedirs("outputs/shoes")
+if not os.path.exists("outputs/pants"):
+    os.makedirs("outputs/pants")
 
-classLabel = net.GetClassLabel(classID)
-confidence *= 100.0
+i = 0
+while True:
+    img = input.Capture()
 
-print(f"clothing type: {confidence:05.2f}% {classLabel}")
+    if img is None:
+        continue
+        
+    classID, confidence = net.Classify(img)
 
-font.OverlayText(img, text=f"{confidence:05.2f}% {classLabel}")
-                         
-output.Render(img)
+    classLabel = net.GetClassLabel(classID)
+    confidence *= 100.0
+
+    print(f"clothing type: {confidence:05.2f}% {classLabel}")
+    font.OverlayText(img, text=f"{confidence:05.2f}% {classLabel}")
+                        
+    output = videoOutput(f"outputs/{classLabel}/output_{i}.jpg", argv=sys.argv)
+    i += 1
+    output.Render(img)
+
+    if not input.IsStreaming() or not output.IsStreaming():
+        break
 
 
